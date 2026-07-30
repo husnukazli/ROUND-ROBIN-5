4a576921f495c64b7a2406c3c6dd63e616ad00ca
husnukazli <husnukazli@gmail.com>
2026-07-30 23:00:26 +0300

    Update roundrobin5player.py

--- roundrobin5player.py
+++ roundrobin5player.py
@@ -39,7 +39,7 @@ def arkaplan_ekle(resim_yolu):
         pass
 # --- GENEL SAYFA AYARLARI ---
 st.set_page_config(page_title="Tenis Turnuva Otomasyonu", page_icon="🎾", layout="wide", initial_sidebar_state="collapsed")
-# arkaplan_ekle("arkaplan.jpg")
+arkaplan_ekle("arkaplan.jpg")
 # --- SUPABASE BAĞLANTISI ---
 @st.cache_resource
 def init_supabase() -> Client:
