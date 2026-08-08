70d17767d1fd4a2ffe0f843dd3116fb4c36dba8a
husnukazli <husnukazli@gmail.com>
2026-08-08 22:20:12 +0300

    Update hesaplama_motoru.py

--- hesaplama_motoru.py
+++ hesaplama_motoru.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+# =====================================================================
+# TENİS HESAPLAMA VE KURAL KONTROL MOTORU
+# =====================================================================
+
+# ANA DOSYADAN KESİLECEK 1. FONKSİYON BURAYA GELECEK:
+# def sort_maclar(df):
+# ...
+
+
+# ANA DOSYADAN KESİLECEK 2. FONKSİYON BURAYA GELECEK:
+# def set_gecerli_mi(s1, s2, is_set3=False, durum="Tamamlandı"):
+# ...
+
+
+# ANA DOSYADAN KESİLECEK 3. FONKSİYON BURAYA GELECEK:
+# def hesapla_mac_kazanani(row):
+# ...
